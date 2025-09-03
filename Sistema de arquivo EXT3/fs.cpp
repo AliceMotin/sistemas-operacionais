37bf1a05586305838007b34a0d693eb453c86695
Alice <alice.bastos@bridge.ufsc.br>
2025-09-03 19:57:26 -0300

    .

---
+++ Sistema de arquivo EXT3/fs.cpp
@@ -0,0 +1,16 @@
+/**
+ * Implemente aqui as funções dos sistema de arquivos que simula EXT3
+ */
+
+#include "fs.h"
+
+void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes){};
+
+void addFile(std::string fsFileName, std::string filePath, std::string fileContent){};
+
+void addDir(std::string fsFileName, std::string dirPath){};
+
+void remove(std::string fsFileName, std::string path){};
+
+void move(std::string fsFileName, std::string oldPath, std::string newPath){};
+
